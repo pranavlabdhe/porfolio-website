431d3fd73e88f1c1995f45db7e1a5eef9a1e083e
pranav <pranav@pranavs-MacBook-Air.local>
2025-05-06 13:40:51 +0530

    Added downloadable CV feature

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -29,8 +29,8 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
 
-              <a
-                href=""
+              <a // Open in new tab
+                href="https://drive.google.com/uc?export=download&id=1B_W6UU50bnToEBUE1Hs2hdwXMy7iMm42"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
